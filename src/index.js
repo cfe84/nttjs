aefe24aeefe8625eace8aadc7fa4cf03dcc7e85a
Charles Feval <github@crap.feval.fr>
2017-05-15 13:25:34 -0700

    Index + its integration tests

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const entity = require("./domain/entity");
+const fsFileAdapter = require("./infrastructure/fsFileAdapter");
+const JSONSerializer = require("./middleware/JSONSerializer");
+
+/**
+ * @type {{ntt: entity, adapters: {fs: fsFileAdapter}}}
+ */
+const ntt =  {
+  ntt: (adapter, serializer = JSONSerializer) => {
+    return entity(adapter, serializer);
+  },
+  adapters: {
+    fs: fsFileAdapter
+  }
+};
+
+module.exports = ntt;
---
+++ test/integration.test.js
@@ -0,0 +1,49 @@
+const ntt = require("../src/index");
+const rmrf = require("rimraf");
+const path = require("path");
+const fs = require("fs");
+const should = require("should");
+
+const prepareTestDir = (directoryPath) => {
+  const SEP = path.sep;
+  const rootDirectory = `${directoryPath}${SEP}fsIntegrationTest`;
+  const resources = {
+    rootDirectory: rootDirectory,
+    entity: { name: "Tomas" },
+    resourcename: `subresource1`,
+    resourceentityId: "3242omfo235rt2t",
+    resourceentity: { name: "Peter" }
+  };
+
+  clearTestDir(rootDirectory);
+  fs.mkdirSync(rootDirectory);
+  fs.writeFileSync(path.join(rootDirectory, "entity.json"), JSON.stringify(resources.entity));
+  fs.mkdirSync(path.join(rootDirectory, resources.resourcename));
+  fs.mkdirSync(path.join(rootDirectory, resources.resourcename, resources.resourceentityId));
+  fs.writeFileSync(path.join(rootDirectory, resources.resourcename, resources.resourceentityId, "entity.json"), JSON.stringify(resources.resourceentity));
+  return resources;
+};
+
+const clearTestDir = (rootDirectory) => {
+  if (fs.existsSync(rootDirectory)){
+    rmrf.sync(rootDirectory);
+  }
+};
+
+describe("FS Integration test", () => {
+  const resources = prepareTestDir("./test");
+
+  const entity = ntt.ntt(ntt.adapters.fs(resources.rootDirectory));
+
+  it("reads entity correctly", () => {
+    return entity.load()
+      .then((ent) => {
+        should(ent).not.be.undefined();
+        ent.name.should.equal(resources.entity.name);
+      });
+  });
+
+  it("creates resources", () => {
+  });
+
+});
