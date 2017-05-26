17094f1b946ceb2ba774a50a14bb90343fff9413
Charles Feval <github@crap.feval.fr>
2017-05-25 17:09:06 -0700

    Removing keys from there

--- src/infrastructure/azureBlobStorageFileAdapter.test.js
+++ src/infrastructure/azureBlobStorageFileAdapter.test.js
@@ -3,17 +3,11 @@ const azure = require("azure-storage");
 const should = require("should");
 
 const TEST_CONTAINER = "ntttest";
-const ACCOUNT = "picklockdemo";
-const KEY = "AOm8hOLH8YOJDtEQO/ZNXVif5q9Q58f56dJnDiErsGgccWfKadzwH5VwQMImu4KyEZtoKFXOdj0tN6NMiUkm6g==";
-
-process.env.AZURE_STORAGE_ACCOUNT = ACCOUNT;
-process.env.AZURE_STORAGE_KEY = KEY;
 
 const config = {
   account: process.env.AZURE_STORAGE_ACCOUNT,
   key: process.env.AZURE_STORAGE_KEY
 };
-
 
 const clearTestContainer = (config, callback) => {
   const blobService = azure.createBlobService(config.account, config.key);
@@ -34,9 +28,14 @@ const clearTestContainer = (config, callback) => {
   });
 };
 
-const adapter = adapterFactory(config, TEST_CONTAINER);
 
 describe("Azure blob storage adapter", () => {
+  if (!config.account || !config.key) {
+    throw Error("You need to define environment variables AZURE_STORAGE_ACCOUNT and AZURE_STORAGE_KEY" +
+      " to define an integration test account");
+  }
+
+  const adapter = adapterFactory(config, TEST_CONTAINER);
 
   before((done) => clearTestContainer(config, done));
 
