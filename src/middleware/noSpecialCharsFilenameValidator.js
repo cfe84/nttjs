64b6f5148165355d8bcbe1e99e7e59b8b2499da0
Charles Feval <github@crap.feval.fr>
2017-06-29 08:23:36 -0700

    Fixing issues with the validator

--- src/middleware/noSpecialCharsFilenameValidator.js
+++ src/middleware/noSpecialCharsFilenameValidator.js
@@ -1,13 +1,27 @@
 const isAString = (folderName) => folderName === `${folderName}`;
-const hasValue = (folderName) => folderName !== null && folderName !== undefined;
+const hasValue = (folderName) => folderName !== null && folderName !== undefined && folderName !== "";
+
+const errors = {
+  empty: "File or folder name is empty",
+  notAString: "File or folder name is not a string",
+  invalidCharacters: "File or folder name contains invalid characters"
+};
 
 const validator = {
   validate: (folderName) => {
     const regex = /^[ a-z0-9_-]+$/i;
-    return hasValue(folderName) &&
-      isAString(folderName) &&
-      regex.test(folderName);
-  }
+    if (!hasValue(folderName)) {
+      return Promise.reject(Error(errors.empty));
+    }
+    if (!isAString(folderName)) {
+      return Promise.reject(Error(errors.notAString));
+    }
+    if (!regex.test(folderName)) {
+      return Promise.reject(Error(errors.invalidCharacters));
+    }
+    return Promise.resolve();
+  },
+  errors: errors
 };
 
 module.exports = validator;
--- src/middleware/noSpecialCharsFilenameValidator.test.js
+++ src/middleware/noSpecialCharsFilenameValidator.test.js
@@ -1,21 +1,32 @@
 const validator = require("./noSpecialCharsFilenameValidator");
+const should = require("should");
 
 describe("Name validation", () => {
-  it("should validate correct names", () => {
-    validator.validate("kdsjfskdjfsk").should.be.true("only lower case characters");
-    validator.validate("kdsjfs kdjfsk").should.be.true("spaces are valid");
-    validator.validate("sdSf992").should.be.true("mix of lower, upper and numbers");
-    validator.validate("subresource1").should.be.true("subresource1");
-    validator.validate("00000000000").should.be.true("string with only zeros");
+  describe("should validate correct names", () => {
+    it("only lower case characters", () => validator.validate("kdsjfskdjfsk"));
+    it("spaces are valid", () => validator.validate("kdsjfs kdjfsk"));
+    it("mix of lower, upper and numbers", () => validator.validate("sdSf992"));
+    it("subresource1", () => validator.validate("subresource1"));
+    it("string with only zeros", () => validator.validate("00000000000"));
   });
-  it("should invalidate incorrect names", () => {
-    validator.validate("sdf/").should.be.false("contains a forward slash");
-    validator.validate("sdf\\*#&").should.be.false("contains special chars");
-    validator.validate("/").should.be.false("only a forward slash");
-    validator.validate("").should.be.false("empty string");
-    validator.validate(1).should.be.false("number");
-    validator.validate(null).should.be.false("null");
-    validator.validate().should.be.false("nothing");
-    validator.validate(undefined).should.be.false("undefined");
+  describe("should invalidate incorrect names", () => {
+    it("contains a forward slash", () => should(validator.validate("sdf/"))
+      .be.rejectedWith(validator.errors.invalidCharacters));
+    it("contains special chars", () => should(validator.validate("sdf\\*#&"))
+      .be.rejectedWith(validator.errors.invalidCharacters));
+    it("only a forward slash", () => should(validator.validate("/"))
+      .be.rejectedWith(validator.errors.invalidCharacters));
+    it("empty string", () => should(validator.validate(""))
+      .be.rejectedWith(validator.errors.empty));
+    it("number", () => should(validator.validate(1))
+      .be.rejectedWith(validator.errors.notAString));
+    it("object", () => should(validator.validate({}))
+      .be.rejectedWith(validator.errors.notAString));
+    it("null", () => should(validator.validate(null))
+      .be.rejectedWith(validator.errors.empty));
+    it("nothing", () => should(validator.validate())
+      .be.rejectedWith(validator.errors.empty));
+    it("undefined", () => should(validator.validate(undefined))
+      .be.rejectedWith(validator.errors.empty));
   });
 });
